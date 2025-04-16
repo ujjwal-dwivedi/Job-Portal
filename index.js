8eda8c3d31aceb08e12193cfc5e2b12670ed2620
ujjwal6bitquasar <ujjwal264dwivedi@gmail.com>
2025-04-16 17:50:17 +0530

    Made .env

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 import express from 'express'
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config({});
 const app=express();
+//  app.get("/home",(req,res)=>{
+//     return res.status(200).json({
+//         message:"I am coming from backend",
+//         "success":true
+//     })
+//  })
+
 //middleware
-app.use(express.json);
+app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+const corsOptions={
+    origin:'http//localhost:5173',
+    credentials:true
+}
+app.use(cors(corsOptions));
 
-
-
-const PORT=3000;
+//DB_ Password=62ghBlAOBfKPW8w2
+//DB_Username=ujjwal264dwivedi
+const PORT=process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log(`server running at port ${PORT}`);
 })
